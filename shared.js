f3d8a804a208c64b5116417346add23f419a7320
keren3491 <keren3491@gmail.com>
2026-07-19 10:37:16 -0400

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,13 +1,11 @@
-window.APP_CONFIG = {
-boardId: "main",
-firebase: {
-apiKey: "AIzaSyD556nw0Y8Lv4oZyQ13IvUQ8fh1Lo710lE",
-authDomain: "mb4t-74974.firebaseapp.com",
-databaseURL: "https://mb4t-74974-default-rtdb.firebaseio.com",
-projectId: "mb4t-74974",
-storageBucket: "mb4t-74974.firebasestorage.app",
-messagingSenderId: "592373466358",
-appId: "1:592373466358:web:b8dfa21efa4a03503398d7",
-measurementId: "G-5PVSVCJ51V"
-}
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD556nw0Y8Lv4oZyQ13IvUQ8fhllo710lE",
+  authDomain: "mb4t-74974.firebaseapp.com",
+  databaseURL: "https://mb4t-74974-default-rtdb.firebaseio.com",
+  projectId: "mb4t-74974",
+  storageBucket: "mb4t-74974.firebasestorage.app",
+  messagingSenderId: "592373466358",
+  appId: "1:592373466358:web:b8dfa21efa4a03503398d7",
+  measurementId: "G-5PVSVCJ51V"
 };
